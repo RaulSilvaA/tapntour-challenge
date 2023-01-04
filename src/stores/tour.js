92c36f64539892185e21917c62c402e29cdd6b6a
Raúl Silva <rausilva@ug.uchile.cl>
2023-01-04 13:19:07 -0300

    ADDED: a way to get the tour details

--- src/stores/tour.js
+++ src/stores/tour.js
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 
 const tour_endpoint = 'https://tapntour-wk-db.glyfo.workers.dev/api/tours'
+const tourDetail_endpoint = 'https://tapntour-wk-db.glyfo.workers.dev/api/tours/detail/'
 
 export const useTourStore = defineStore({
   id: 'tour',
@@ -33,8 +34,8 @@ export const useTourStore = defineStore({
       this.tour = null
       this.loading = true
       try {
-        this.post = await axios.post(tourbyid_endpoint)
-        .then((response) => response)
+        this.tour = await axios.post(tourDetail_endpoint + id)
+        .then((response) => response.data)
       } catch (error) {
         this.error = error
       } finally {
